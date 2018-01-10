2bb39aefc7512bbbb3b97f1644196cb29f80bbe5
irefantasy@126.com <309741309@qq.com>
2018-01-10 21:30:59 +0800

    SimpleOS

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # SimpleOS
-~
+
+读取的柱面个数保存地址 [0x0ff0]
+
+16位寄存器:
+
+- AX accumulator,累加寄存器
+- CX counter,计数寄存器
+- DX data,数据寄存器
+- BX base,基址寄存器
+- SP stack pointer,栈指针寄存器
+- BP base pointer,基址指针寄存器
+- SI source index,源变址寄存器
+- DI destination index,目的变址寄存器
+
+8位寄存器:
+
+- AL 累加寄存器低位
+- CL 计数寄存器低位
+- DL 数据寄存器低位
+- BL 基址寄存器低位
+- AH 累加寄存器高位
+- CH 计数寄存器高位
+- DH 数据寄存器高位
+- BH 基址寄存器高位
+
+32位寄存器:
+
+- EAX accumulator,累加寄存器
+- ECX counter,计数寄存器
+- EDX data,数据寄存器
+- EBX base,基址寄存器
+- ESP stack pointer,栈指针寄存器
+- EBP base pointer,基址指针寄存器
+- ESI source index,源变址寄存器
+- EDI destination index,目的变址寄存器
+
+段寄存器：
+
+- ES 附加段寄存器
+- CS 代码段寄存器
+- SS 栈段寄存器
+- DS 数据段寄存器
+- FS 没有名称
+- GS 没有名称
+
+- JMP 跳转指令
+- CMP 比较指令
+- JE 条件跳转
+```
+// CMP AL,0
+// JE fin
+if(AL == 0)
+{
+    goto fin;
+}
+```
---
+++ SimpleOS/fifo.c
@@ -0,0 +1,58 @@
+/* FIFO通用缓冲 */
+
+#include "bootpack.h"
+
+#define FLAGS_OVERRUN		0x0001
+
+void fifo8_init(struct FIFO8 *fifo, int size, unsigned char *buf)
+{
+	fifo->size = size;
+	fifo->buf = buf;
+	fifo->free = size; /* 缓冲区的剩余大小 */
+	fifo->flags = 0;
+	fifo->p = 0; /* 下一个写入地址 */
+	fifo->q = 0; /* 下一个读取地址 */
+	return;
+}
+
+//写入缓冲
+int fifo8_put(struct FIFO8 *fifo, unsigned char data)
+{
+	if (fifo->free == 0) 
+	{
+		fifo->flags |= FLAGS_OVERRUN;
+		return -1;
+	}
+	fifo->buf[fifo->p] = data;
+	fifo->p++;
+	if (fifo->p == fifo->size) 
+	{
+		fifo->p = 0;
+	}
+	fifo->free--;
+	return 0;
+}
+
+//读取缓冲
+int fifo8_get(struct FIFO8 *fifo)
+{
+	int data;
+	if (fifo->free == fifo->size) 
+	{
+		//溢出
+		return -1;
+	}
+	data = fifo->buf[fifo->q];
+	fifo->q++;
+	if (fifo->q == fifo->size) 
+	{
+		fifo->q = 0;
+	}
+	fifo->free++;
+	return data;
+}
+
+int fifo8_status(struct FIFO8 *fifo)
+{
+	return fifo->size - fifo->free;
+}
